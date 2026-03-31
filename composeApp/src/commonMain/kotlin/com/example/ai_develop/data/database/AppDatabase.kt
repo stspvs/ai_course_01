f8d64846ccabe2430fc641a5c56767448bd421b0
Полозов Станислав <stanislav.polozov@gazprombank.ru>
2026-03-31 14:26:17 +0300

    БД версия

--- composeApp/src/commonMain/kotlin/com/example/ai_develop/data/database/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/example/ai_develop/data/database/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
 import androidx.room.TypeConverters
 
-@Database(entities = [AgentEntity::class, MessageEntity::class], version = 3) // Поднял версию до 3 для сброса данных
+@Database(entities = [AgentEntity::class, MessageEntity::class], version = 5)
 @TypeConverters(Converters::class)
 @ConstructedBy(AppDatabaseConstructor::class)
 abstract class AppDatabase : RoomDatabase() {
